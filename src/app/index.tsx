d26e7a380de59248c01e426b34c1b6b3a12d919a
SSen <i@ssen.name>
2019-08-25 20:52:38 +0900

    feat: circle → travis feat: cypress → puppeteer ...and more

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,10 +2,6 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Title } from './components/Title';
 
-require('react-app-polyfill/ie11');
-
-require('./style.scss');
-
 function App() {
   return (
     <Title text="Hello World!"/>
