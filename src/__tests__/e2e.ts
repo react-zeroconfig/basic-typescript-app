a668ec7bb3d9bd87d79a5d3da4918621e80d30fd
SSen <i@ssen.name>
2020-04-22 16:32:47 +0900

    chore: prettier format

--- src/__tests__/e2e.ts
+++ src/__tests__/e2e.ts
@@ -8,7 +8,7 @@ describe('E2E Sample', () => {
     await page.goto('http://localhost:3100');
     await page.waitForSelector('#app > h1');
 
-    await expect(page.$eval('#app > h1', e => e.innerHTML)).resolves.toEqual('Hello World!');
+    await expect(page.$eval('#app > h1', (e) => e.innerHTML)).resolves.toEqual('Hello World!');
 
     await browser.close();
   });
